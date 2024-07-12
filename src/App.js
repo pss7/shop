03fe94d37f44dd4c7915155baad7be1598993811
parksangseok <>
2024-07-12 22:58:20 +0900

    코드 길어지면 import export

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import './App.css';
 import bg from './img/bg.png';
+// import a from './data.js';
+// import { a, b } from './data.js';
+import data from './data.js';
+import { useState } from 'react';
 
 function App() {
+
+  let [ shoes ] = useState(data);
+
   return (
     <div className="App">
+
       <Navbar bg="light" variant="light">
         <Container>
           <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
@@ -21,18 +29,18 @@ function App() {
         <div className="row">
           <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[0].title}</h4>
+            <p>{shoes[0].price}</p>
           </div>
           <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[1].title}</h4>
+            <p>{shoes[1].price}</p>
           </div>
           <div className="col-md-4">
             <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" />
-            <h4>상품명</h4>
-            <p>상품정보</p>
+            <h4>{shoes[2].price}</h4>
+            <p>{shoes[2].price}</p>
           </div>
         </div>
       </div>
