17fe28b7f9efc9fa97ac16f2c714570c4069efcf
parksangseok <>
2024-07-14 16:57:07 +0900

    Lifecycle과 useEffect 1

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,4 @@
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
-
-let YellowBtn = styled.button`
- background: ${props => props.bg};
- color: ${props => props.bg == "blue" ? "white" : "black"};
- padding: 10px;
-`
-
-/*
-let Box = styled.div`
-background: grey;
-padding: 20px;
-`
-*/
 
 function Detail(props) {
 
@@ -25,12 +11,6 @@ function Detail(props) {
     return (
         <>
             <div className="container">
-
-                {/* <Box></Box> */}
-
-                <YellowBtn bg="blue">버튼</YellowBtn>
-                <YellowBtn bg="orange">버튼</YellowBtn>
-
                 <div className="row">
                     <div className="col-md-6">
                         <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
