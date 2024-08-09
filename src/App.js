ae2c1dc9bd62865b380d46cac520b5be56804903
parksangseok <>
2024-08-09 11:45:07 +0900

    성능개선 1 : 개발자도구 & lazy import

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import './App.css';
 import bg from './img/bg.png';
 import data from './data.js';
-import { createContext, useEffect, useState } from 'react';
+import { createContext, lazy, useEffect, useState, Suspense } from 'react';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
-import Detail from './routes/Detail.js';
-import Cart from './routes/Cart.js';
 import axios from 'axios';
 import { BarLoader } from 'react-spinners';
 import { useQuery } from 'react-query';
+
+// import Detail from './routes/Detail.js';
+// import Cart from './routes/Cart.js';
+
+const Detail = lazy(() => import('./routes/Detail.js'))
+const Cart = lazy(() => import('./routes/Cart.js'))
 
 export let Context1 = createContext();
 
@@ -26,7 +30,7 @@ function App() {
       return a.data
     }),
     {
-      staleTime : 2000
+      staleTime: 2000
     }
   })
 
@@ -69,101 +73,103 @@ function App() {
         </Container>
       </Navbar>
 
-      <Routes>
-        <Route path="/" element={
-          <>
-            <div className="main-bg" style={{ backgroundImage: 'url(' + bg + ')' }}></div>
-            <div className="container">
-              <div className="row">
+      <Suspense fallback={<div>로딩중</div>}>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <div className="main-bg" style={{ backgroundImage: 'url(' + bg + ')' }}></div>
+              <div className="container">
+                <div className="row">
 
-                {
-                  shoes.map((a, i) => {
-                    return (
-                      <Card key={i} shoes={shoes[i]} i={i} />
-                    )
-                  })
-                }
+                  {
+                    shoes.map((a, i) => {
+                      return (
+                        <Card key={i} shoes={shoes[i]} i={i} />
+                      )
+                    })
+                  }
 
+                </div>
               </div>
-            </div>
-            <button onClick={() => {
+              <button onClick={() => {
 
-              setLoading(true);
-              setBtnNum(btnNum + 1);
+                setLoading(true);
+                setBtnNum(btnNum + 1);
 
-              /*
-if (btnNum == 1) {
-  axios.get('https://codingapple1.github.io/shop/data2.json')
-    .then((결과) => {
-      let copy = [...shoes, ...결과.data];
-      setShoes(copy);
-      setLoading(false);
-    })
-    .catch(() => {
-      console.log('실패함');
-      setLoading(false);
-    })
-} else if (btnNum == 2) {
-  axios.get('https://codingapple1.github.io/shop/data3.json')
-    .then((결과) => {
-      let copy = [...shoes, ...결과.data];
-      setShoes(copy);
-      setLoading(false);
-    })
-    .catch(() => {
-      console.log('실패함');
-      setLoading(false);
-    })
-} else {
-  alert('상품이 없습니다.');
-  setLoading(false);
-}
-*/
+                /*
+  if (btnNum == 1) {
+    axios.get('https://codingapple1.github.io/shop/data2.json')
+      .then((결과) => {
+        let copy = [...shoes, ...결과.data];
+        setShoes(copy);
+        setLoading(false);
+      })
+      .catch(() => {
+        console.log('실패함');
+        setLoading(false);
+      })
+  } else if (btnNum == 2) {
+    axios.get('https://codingapple1.github.io/shop/data3.json')
+      .then((결과) => {
+        let copy = [...shoes, ...결과.data];
+        setShoes(copy);
+        setLoading(false);
+      })
+      .catch(() => {
+        console.log('실패함');
+        setLoading(false);
+      })
+  } else {
+    alert('상품이 없습니다.');
+    setLoading(false);
+  }
+  */
 
-              axios.get("https://codingapple1.github.io/shop/data" + (btnNum + 2) + ".json")
-                .then((결과) => {
-                  let copy = [...shoes, ...결과.data];
-                  setShoes(copy);
-                  setLoading(false);
-                })
-                .catch(() => {
-                  setLoading(false);
-                  alert('상품이 없습니다.');
-                })
+                axios.get("https://codingapple1.github.io/shop/data" + (btnNum + 2) + ".json")
+                  .then((결과) => {
+                    let copy = [...shoes, ...결과.data];
+                    setShoes(copy);
+                    setLoading(false);
+                  })
+                  .catch(() => {
+                    setLoading(false);
+                    alert('상품이 없습니다.');
+                  })
 
-            }}>버튼</button>
+              }}>버튼</button>
 
-            {
-              loading === true ? <Loading /> : null
-            }
+              {
+                loading === true ? <Loading /> : null
+              }
 
-            <ProductList shoes={shoes} />
+              <ProductList shoes={shoes} />
 
-          </>
+            </>
 
-        }
+          }
 
-        ></Route>
-        <Route path="/detail/:id" element={
-          <Context1.Provider value={{ 재고 }}>
-            <Detail shoes={shoes} />
-          </Context1.Provider>
-        } />
+          ></Route>
+          <Route path="/detail/:id" element={
+            <Context1.Provider value={{ 재고 }}>
+              <Detail shoes={shoes} />
+            </Context1.Provider>
+          } />
 
-        <Route path="/cart" element={<Cart />} />
+          <Route path="/cart" element={<Cart />} />
 
-        <Route path="/about" element={<About />} >
-          <Route path="member" element={<div>멤버임</div>} />
-          <Route path="location" element={<div>위치정보임</div>} />
-        </Route>
+          <Route path="/about" element={<About />} >
+            <Route path="member" element={<div>멤버임</div>} />
+            <Route path="location" element={<div>위치정보임</div>} />
+          </Route>
 
-        <Route path="/event" element={<EventPage />}>
-          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
-          <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
-        </Route>
+          <Route path="/event" element={<EventPage />}>
+            <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
+            <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
+          </Route>
 
-        <Route path='*' element={<div>404</div>} />
-      </Routes>
+          <Route path='*' element={<div>404</div>} />
+        </Routes>
+      </Suspense>
 
     </div>
   );
